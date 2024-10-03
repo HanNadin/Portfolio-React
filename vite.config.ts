a0c4a3e943efc956813b5f78117c08e62a03d29a
HanNadine <eranadine3540@gmail.com>
2024-10-03 14:10:06 +0700

    changes1

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Portfolio-React/', // Set the correct base URL
   plugins: [react()],
 })
